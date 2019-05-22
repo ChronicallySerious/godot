44103dae414fedb30c982e36aa43c2d8f5431ff6
Twarit <wtwarit@gmail.com>
2019-05-23 00:43:46 +0530

    Add popup windown size logic

--- editor/vcs_settings_editor.cpp
+++ editor/vcs_settings_editor.cpp
@@ -1,23 +1,47 @@
+#include "editor_settings.h"
 #include "vcs_settings_editor.h"
+
+float VCSSettingsEditor::scale;
 
 void VCSSettingsEditor::_initialise_vcs_plugin(String p_vcs_name) {
 
 	EditorSettings::get_singleton()->set_project_metadata("vcs", "name", p_vcs_name);
-
-	
-}
-
-void VCSSettingsEditor::_bind_methods() {
-
-	ClassDB::bind_method(D_METHOD("_initialise_vcs_plugin"), &VCSSettingsEditor::_initialise_vcs_plugin);
+	scale = 2.0f;
 }
 
 void VCSSettingsEditor::popup_vcs_settings() {
 
-	Rect2 saved_size = EditorSettings::get_singleton()->get_project_metadata("dialog_bounds", "vcs_settings", Rect2());
+	Size2 popup_size = Size2(900, 700) * editor_get_scale();
+	Size2 window_size = get_viewport_rect().size;
+
+	popup_size.x = MIN(window_size.x * 0.5, popup_size.x);
+	popup_size.y = MIN(window_size.y * 0.5, popup_size.y);
 	
-	if (saved_size != Rect2())
-	{
-		popup(saved_size);
-	}
+	popup_centered(popup_size);
+}
+
+float VCSSettingsEditor::editor_get_scale() {
+	return scale;
+}
+
+VCSSettingsEditor::VCSSettingsEditor(EditorData *p_data) {
+	set_title(TTR("VCS Settings (project.godot)"));
+	set_resizable(true);
+	data = p_data;
+
+	VBoxContainer *vbc = memnew(VBoxContainer);
+	vbc->set_alignment(BoxContainer::ALIGN_BEGIN);
+	vbc->set_v_size_flags(Control::SIZE_EXPAND_FILL);
+	add_child(vbc);
+
+	HBoxContainer *hbc = memnew(HBoxContainer);
+	hbc->set_h_size_flags(Control::SIZE_EXPAND_FILL);
+	vbc->add_child(hbc);
+
+	label = memnew(OptionButton);
+	label->set_text(TTR("Version Control Setting"));
+	vbc->add_child(label);
+}
+
+VCSSettingsEditor::~VCSSettingsEditor() {
 }
--- editor/vcs_settings_editor.h
+++ editor/vcs_settings_editor.h
@@ -2,24 +2,29 @@
 #define VERSION_CONTROL_SETTINGS_EDITOR_H
 
 #include "core/array.h"
+#include "editor/editor_data.h"
 #include "scene/gui/dialogs.h"
-#include "editor_settings.h"
 
 class VCSSettingsEditor : public AcceptDialog {
 	GDCLASS(VCSSettingsEditor, AcceptDialog);
 
-	Array available_vcs_plugins_names;
-	String selected_vcs;
+	String vcs_name;
+	EditorData *data;
+
+	OptionButton *label;
+
+	static float scale;
 
 	static void _initialise_vcs_plugin(String p_vcs_name);
-
-	protected:
-	static void _bind_methods();
 
 	public:
 	void popup_vcs_settings();
 
-	String get_vcs_type() { return selected_vcs; }
+	float editor_get_scale();
+	String get_vcs_name() { return vcs_name; }
+
+	VCSSettingsEditor(EditorData *p_data);
+	~VCSSettingsEditor();
 };
 
 #endif
