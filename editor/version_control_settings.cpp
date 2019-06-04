8b766b2ee018aca054ba1c1d43b9be84f8a44190
Twarit <wtwarit@gmail.com>
2019-06-05 00:41:19 +0530

    Refactor VCS plugin and separate out VCS settings This currently fails to create a GUI hierarchy correctly because it is using the free refactor children structure

---
+++ editor/version_control_settings.cpp
@@ -0,0 +1,11 @@
+#include "editor_settings.h"
+#include "version_control_settings.h"
+
+void VersionControlSettings::_bind_methods() {
+}
+
+VersionControlSettings::VersionControlSettings(EditorData *p_data) {
+}
+
+VersionControlSettings::~VersionControlSettings() {
+}
