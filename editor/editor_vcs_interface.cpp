2da4226047a011d7b9b797cc9ba658b293924758
Twarit <wtwarit@gmail.com>
2019-07-10 01:01:42 +0530

    Reset to in source plugin

--- editor/editor_vcs_interface.cpp
+++ editor/editor_vcs_interface.cpp
@@ -1,14 +1,11 @@
 #include "editor/editor_vcs_interface.h"
 
 EditorVCSInterface *EditorVCSInterface::singleton;
-VCSInterface *vcs_api_struct = NULL;
 
 void EditorVCSInterface::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("register_vcs_addon", "vcs_name"), &EditorVCSInterface::register_vcs_addon);
 
-	ClassDB::bind_method(D_METHOD("get_singleton"), &EditorVCSInterface::get_singleton_node);
-	ClassDB::bind_method(D_METHOD("set_api_struct", "vcs_api_struct"), &EditorVCSInterface::set_api_struct);
 	ClassDB::bind_method(D_METHOD("set_version_control_dock", "vcs_control_dock"), &EditorVCSInterface::set_version_control_dock);
 	ClassDB::bind_method(D_METHOD("set_version_commit_dock", "version_commit_dock"), &EditorVCSInterface::set_version_commit_dock);
 	ClassDB::bind_method(D_METHOD("set_version_control_name", "vcs_name"), &EditorVCSInterface::set_version_control_name);
@@ -17,15 +14,6 @@ void EditorVCSInterface::_bind_methods() {
 bool EditorVCSInterface::register_vcs_addon(String p_vcs_name) {
 
 	return VersionControlEditorPlugin::get_singleton()->register_as_available_vcs(p_vcs_name);
-}
-
-void EditorVCSInterface::set_api_struct(Node *p_vcs_api_struct) {
-
-	vcs_api_struct = Object::cast_to<VCSInterface>(p_vcs_api_struct);
-	if (vcs_api_struct) {
-
-		WARN_PRINT("vcs_api_struct is set to NULL");
-	}
 }
 
 void EditorVCSInterface::set_version_control_dock(Node *p_vcs_control_dock) {
@@ -57,7 +45,7 @@ void EditorVCSInterface::set_version_control_name(String p_vcs_name) {
 
 String EditorVCSInterface::get_vcs_name() {
 
-	return vcs_api_struct != NULL ? vcs_api_struct->get_vcs_name() : "Default";
+	return "Default";
 }
 
 EditorVCSInterface::EditorVCSInterface() {
--- editor/editor_vcs_interface.h
+++ editor/editor_vcs_interface.h
@@ -5,22 +5,11 @@
 #include "core/object.h"
 #include "editor/plugins/version_control_editor_plugin.h"
 
-struct VCSInterface {
-
-	void (*constructor)();
-	void (*destructor)();
-
-	String (*get_vcs_name)();
-};
-
-extern VCSInterface *vcs_api_struct;
-
 class EditorVCSInterface : public Object {
 
 	GDCLASS(EditorVCSInterface, Object)
 
 	static EditorVCSInterface *singleton;
-	static EditorVCSInterface *vcs_interface;
 	
 	friend class VersionControlEditorPlugin;
 
@@ -29,16 +18,15 @@ protected:
 
 public:
 	static EditorVCSInterface *get_singleton() { return singleton; }
-	static Node *get_singleton_node() { return (Node *)singleton; }
+
 	bool register_vcs_addon(String p_vcs_name);
 
-	void set_api_struct(Node *p_vcs_api_struct);
 	void set_version_control_dock(Node *p_vcs_control_dock);
 	void set_version_commit_dock(Node *p_vcs_commit_dock);
 	void set_version_control_name(String p_vcs_name);
 
 	// Exposed VCS access functions to editor
-	String get_vcs_name();
+	virtual String get_vcs_name();
 	
 	EditorVCSInterface();
 	virtual ~EditorVCSInterface();
