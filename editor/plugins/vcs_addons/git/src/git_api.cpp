06c9dd4bdbba6d5b6175f224c261d446c9b1b57c
Twarit <wtwarit@gmail.com>
2019-07-15 20:48:25 +0530

    Link libgit2 to godot

--- editor/plugins/vcs_addons/git/src/git_api.cpp
+++ editor/plugins/vcs_addons/git/src/git_api.cpp
@@ -1,4 +1,13 @@
 #include "git_api.h"
+#include "editor/editor_vcs_interface.h"
+
+//temporary
+extern "C" {
+#pragma comment(lib, "winhttp")
+#pragma comment(lib, "Rpcrt4")
+#pragma comment(lib, "CRYPT32")
+#include "thirdparty/libgit2/include/git2.h"
+}
 
 String GitAPI::get_vcs_name() {
 
